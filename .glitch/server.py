989e38dc0f055fa98d29b670a2d73915f9652f20
Saleem Rashid <dev@saleemrashid.com>
2018-05-27 20:26:21 +0100

    glitch: Add keep-alive thread to server

--- .glitch/server.py
+++ .glitch/server.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python3
+import time
+import threading
+import urllib.request
+
 from http.server import BaseHTTPRequestHandler, HTTPServer
+
+
+MESSAGE = b"PONG"
+INTERVAL = 30
 
 
 class RequestHandler(BaseHTTPRequestHandler):
@@ -8,13 +16,25 @@ class RequestHandler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.end_headers()
 
-        self.wfile.write(b"OK")
+        self.wfile.write(MESSAGE)
+
+
+def keep_alive(url):
+    while True:
+        time.sleep(INTERVAL)
+
+        with urllib.request.urlopen(url) as fp:
+            assert fp.read() == MESSAGE
 
 
 if __name__ == "__main__":
     import os
 
-    port = int(os.environ["PORT"])
+    url = "https://{}.glitch.me".format(os.environ["PROJECT_DOMAIN"])
+    thread = threading.Thread(target=keep_alive, args=(url,))
+    thread.daemon = True
+    thread.start()
 
+    port = int(os.environ["PORT"])
     server = HTTPServer(("", port), RequestHandler)
     server.serve_forever()
