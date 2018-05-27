40f76c40ca58b4a016ef677cd5594ad7db50d618
Saleem Rashid <dev@saleemrashid.com>
2018-05-27 18:15:01 +0100

    glitch: Add Source Han Sans
    Fixes #1

--- start.sh
+++ start.sh
@@ -11,7 +11,7 @@ pip3 install -U --user -r ../requirements.txt
 
 # Import font
 mkdir -p ~/.local/share/fonts
-cp SourceSansPro-Bold.otf ~/.local/share/fonts
+cp SourceSansPro-Bold.otf SourceHanSans-Bold.ttc ~/.local/share/fonts
 fc-cache -f
 
 exec supervisord -n
